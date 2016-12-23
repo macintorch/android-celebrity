0b6385bec55b5d1aa9e3c604e1ea3df6c8ef1a2a
Ainor Syahrizal <macintorch@gmail.com>
2016-12-23 09:14:07 +0800

    test button pressed

--- app/src/main/java/ainor/com/my/celebritylist/MainActivity.java
+++ app/src/main/java/ainor/com/my/celebritylist/MainActivity.java
@@ -2,12 +2,30 @@ package ainor.com.my.celebritylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    public void chooseAnswer(View view) {
+        Log.i("Button pressed", "Pressed");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
     }
 }
