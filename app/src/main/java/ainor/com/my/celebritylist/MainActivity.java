0cf5a4c8112b51f1b913d0386412b973da2f6312
Ainor Syahrizal <macintorch@gmail.com>
2016-12-23 19:46:18 +0800

    store celeb names in arraylist

--- app/src/main/java/ainor/com/my/celebritylist/MainActivity.java
+++ app/src/main/java/ainor/com/my/celebritylist/MainActivity.java
@@ -11,11 +11,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
 
     public class DownloadTask extends AsyncTask<String,Void,String> {
 
@@ -84,15 +90,20 @@ public class MainActivity extends AppCompatActivity {
             Matcher m = p.matcher(splitResult[0]);
 
             while (m.find()) {
-                System.out.println(m.group(1));
+                celebURLs.add(m.group(1));
             }
 
             p = Pattern.compile("alt=\"(.*?)\"");
             m = p.matcher(splitResult[0]);
 
             while (m.find()) {
-                System.out.println(m.group(1));
+                celebNames.add(m.group(1));
             }
+
+
+            Random random = new Random();
+
+            chosenCeleb = random.nextInt(celebURLs.size());
 
 
 
